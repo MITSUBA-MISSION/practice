6e8133653fccb1230c46373011d11fc6be6c348b
yoshiharu tsuzuki <tsuzuki.yoshiharu572@mail.kyutech.jp>
2025-02-07 17:41:30 +0900

    commit tsuzuki

--- test.py
+++ test.py
@@ -1,2 +1,2 @@
-name = ["Ozoe", "Fujiwara", "nagata"]
-number = ["231A2043", "231A2138", "231A3094"]
+name = ["Ozoe", "Fujiwara", "nagata","Tsuzuki"]
+number = ["231A2043", "231A2138", "231A3094","241A2100"]
